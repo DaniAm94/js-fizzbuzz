a81c8dabee25a7cf04ee97b23e76de59cd0a5d85
DaniAm94 <dani.amico94@gmail.com>
2023-12-20 15:50:48 +0100

    added minimal js code to solve the exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('JS OK!');
+// Itero tutti i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+
+    //Creo la variabile che contiene quello che verrà stampato
+    // Caso banale: verra stampato il numero se questo non è divisibile ne per 3 ne per 5
+    let stampa = i;
+
+    //Controllo se è divisibile per 3
+    if (!(i % 3)) {
+        //Controllo se è contemporaneamente divisibile per 5
+        if (!(i % 5)) {
+            // Se si stampa FizzBuzz
+            stampa = 'FizzBuzz';
+        } else {
+            // Se non lo è stampa Fizz
+            stampa = 'Fizz';
+        }
+        // Se non è divisibile per 3, controllo che sia divisibile per 5
+    } else if (!(i % 5)) {
+        // Se si stampa Buzz
+        stampa = 'Buzz';
+    }
+
+    // Stampa
+    console.log(stampa);
+}
